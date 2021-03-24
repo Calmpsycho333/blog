0e7a1d468668b79ca2014847607da97150dfa2f8
Louis <viewsdevelop@gmail.com>
2021-03-24 14:48:53 +0000

    Modify order of routes for Devise functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+    resources :posts
+    
     devise_scope :user do
         root to: "devise/sessions#new"
     end
 
     devise_for :users, controllers: { sessions: "users/sessions" }
-
-    resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
